c5f75bfb4fe4eb0700c59d702185b4c8ce55ede3
khanhdzx <khanhvannguyen2303@gmail.com>
2022-09-30 07:59:17 +0700

    ja

---
+++ src/Bai1.java
@@ -0,0 +1,7 @@
+public class Bai1 {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        System.out.println("Hinh tron co ban kinh : " +circle.getRadius());
+        System.out.println("Dien tich hinh tron la : "+circle.getArea());
+    }
+}
---
+++ src/Bai2.java
@@ -0,0 +1,10 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println("Truoc khi gan : "+student);
+        student.setName("Khanh Sky");
+        student.setClasses("JV082022");
+        System.out.println("Sau khi gan : "+student);
+
+    }
+}
